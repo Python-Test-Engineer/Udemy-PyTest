f18fb90040fceb8a0674f17b3517ba30cebff622
Craig West <iwswordpress@gmail.com>
2024-05-11 19:14:34 +0100

    plugin

--- SECTION_06_PLUGINS/pytest_sort.py
+++ SECTION_06_PLUGINS/pytest_sort.py
@@ -2,27 +2,35 @@
 Plugin code
 """
 
+import pytest
+
 __version__ = "0.0.1"
 
-# ===============================================================================
-# Paste contents of the conftest.py file here with any imports as needed
+
+#
+def pytest_addoption(parser):
+    parser.addoption("--desc", action="store_true", default=False)
+    parser.addoption("--runenv", action="store", default="local")
 
 
-def pytest_configure(config):
-    config.addinivalue_line("markers", "slow: mark test as slow to run")
+@pytest.fixture(scope="session")
+def desc(request):
+    if request.config.option.desc:
+        print("\n--desc TRUE: ", request.config.option.desc)
+        return True
+    else:
+        print("\n--desc FALSE: ", request.config.option.desc)
+        return False
 
 
 # A pytest hook to for modifying collected items
-def pytest_collection_modifyitems(items, config):
+def pytest_collection_modifyitems(items, config, desc):
     # we can sort order of items (tests) as needed
     # can ber used to sort by fixtures used if one is say expensive in time
     # items.sort(key=lambda item: "expensive" in item.fixturenames)
-    items.sort(key=lambda item: item.nodeid.split("::")[-1])
-    output = ""
-    print("\n")
-    for test in items:
-        # print(f"Item: {item.nodeid}")
-        keywords = [str(x) for x in test.keywords]
-        all_keywords = ("|").join(keywords)
-        output += f"TEST: {test.nodeid} \n\tKEYWORDS: {all_keywords}\n"
-        print(output)
+    if desc:
+        print(f"\n--desc TRUE: {config.option.desc}")
+        items.sort(key=lambda item: item.nodeid.split("::")[-1], reverse=True)
+    else:
+        print(f"\n--desc FALSE: {config.option.desc}")
+        items.sort(key=lambda item: item.nodeid.split("::")[-1])
