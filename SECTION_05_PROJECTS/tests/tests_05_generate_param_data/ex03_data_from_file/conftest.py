4062b295d78792105cf20ca44161861fb3808cb8
Craig West <iwswordpress@gmail.com>
2024-05-16 15:09:59 +0100

    generate tests

--- SECTION_05_PROJECTS/tests/tests_05_generate_param_data/ex03_data_from_file/conftest.py
+++ SECTION_05_PROJECTS/tests/tests_05_generate_param_data/ex03_data_from_file/conftest.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from _pytest.nodes import Item
 from _pytest.runner import CallInfo
 
+
 print("\n\n")
 
 
@@ -11,3 +12,22 @@ def pytest_addoption(parser):
     parser.addoption(
         "--input_file", action="store", default="", help="Choose an input file!"
     )
+
+
+# A pytest hook to dynamically parametrize tests
+def pytest_generate_tests(metafunc):
+    # Check if the num fixture is used
+    if "initial_value" in metafunc.fixturenames:
+        # Get the command line option
+        input_file = metafunc.config.getoption("--input_file")
+
+        # Read the data from the input file (if specified)
+        # We can load data from any external source that Python supports
+        data = []
+        if input_file:
+            with open(input_file, "r") as f:
+                lines = f.readlines()
+            data = [line.strip() for line in lines]
+
+        # Parametrize the test
+        metafunc.parametrize("initial_value", data)
--- SECTION_05_PROJECTS/tests/tests_05_generate_param_data/ex03_data_from_file/data_from_file_README.md
+++ SECTION_05_PROJECTS/tests/tests_05_generate_param_data/ex03_data_from_file/data_from_file_README.md
@@ -1,5 +1,30 @@
-The data folder is the location of data files.
+The data folder is the location of data file data.txt with contents:
+
+```
+Apples
+Bananas
+Cherries
+Lemons
+Oranges
+```
+
+We use:
+
+```
+pytest_generate_tests(metafunc)
+```
+to dynamically add parameters.
+
+In our test we use a fixture `initial_value` as we used `metafunc.parametrize("initial_value", data):` in our .
+
+```
+def test_read_from_file(initial_value):
+    # get value from file
+    result = initial_value
+    assert result == initial_value
+
 
 We can change source of data to reading a CSV file or geting data from the database.
 
  `python -m pytest -s .\tests\tests_05_generate_param_data\ex03_data_from_file\ --input_file=.\tests\tests_05_generate_param_data\ex03_data_from_file\data\data.txt`
+
