072a94475b58d863a129c97f74639060562c22cf
Craig West <iwswordpress@gmail.com>
2024-05-24 08:25:10 +0100

    notes

--- SECTION_05_PROJECTS/tests/tests_01_collect_tests/test_collect_tests.py
+++ SECTION_05_PROJECTS/tests/tests_01_collect_tests/test_collect_tests.py
@@ -1,21 +1,6 @@
 import pytest
 
 
-def test_interface_simple():
-    assert 1
-
-
-@pytest.mark.outer
-@pytest.mark.inner
-def test_interface_complex():
-    assert 1
-
-
-@pytest.mark.first
-def test_event_simple():
-    assert 1
-
-
 @pytest.mark.outer
 @pytest.mark.first
 @pytest.mark.inner
