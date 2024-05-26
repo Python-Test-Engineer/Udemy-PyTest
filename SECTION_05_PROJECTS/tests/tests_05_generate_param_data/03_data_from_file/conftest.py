9de3b22e8aaac0cf9d494da53e8730f90e00f61d
Craig West <iwswordpress@gmail.com>
2024-05-26 06:01:09 +0100

    generate from file

--- SECTION_05_PROJECTS/tests/tests_05_generate_param_data/03_data_from_file/conftest.py
+++ SECTION_05_PROJECTS/tests/tests_05_generate_param_data/03_data_from_file/conftest.py
@@ -25,5 +25,21 @@ def pytest_addoption(parser):
 
 
 def pytest_generate_tests(metafunc):
+
+    # Check if the num fixture is used
     if "file" in metafunc.fixturenames:
         metafunc.parametrize("file", metafunc.config.getoption("file"))
+
+        # Get the command line option
+        input_file = metafunc.config.getoption("--file")
+        print("input_file: ", input_file)
+        # Read the data from the input file (if specified)
+        # We can load data from any external source that Python supports
+        # data = ["Apples", "Bananas", "Cherries"]
+        if input_file:
+            with open(input_file, "r") as f:
+                lines = f.readlines()
+            data = [line.strip() for line in lines]
+
+        # Parametrize the test
+        metafunc.parametrize("initial_value", data)
